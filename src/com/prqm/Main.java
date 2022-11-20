e38eb3eb31ce07fbf8e5afaa165f8cbf47f71f9e
prqm <pyt_prqingming@163.com>
2022-11-20 22:28:12 +0800

    Initial commit

---
+++ src/com/prqm/Main.java
@@ -0,0 +1,13 @@
+package com.prqm;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("1");
+    }
+
+    public void test(){
+        System.out.println(",.；，");
+    }
+}
